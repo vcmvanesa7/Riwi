7cbb0c2505d7bc63376578ba07b149ba8f9de9a0
Vanesa Carrillo <vcmvanesamedina7@gmail.com>
2025-07-11 00:27:57 -0500

    Delete desarrollo/ruta-basica/M3/main.js

--- desarrollo/ruta-basica/M3/main.js
+++
@@ -1,7 +0,0 @@
-// This is an asynchronous function 
-async function loadPartial(path) {
-    const res = await fetch(path);     // Wait while it finds the path and fetches the file.
-    const html = await res.text();      //Create a new variable called html and wait for the server response to be converted to text
-    document.getElementById("app").innerText = html;
-    //Find the element in the document that has the id app and replace its content with the HTML we just downloaded.
-}
